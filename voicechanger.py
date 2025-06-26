c643193b757e0609bea4ab78e52de50496570c35
AmpilGit <82020458+AmpilGit@users.noreply.github.com>
2025-06-26 14:48:13 +0300

    Update voicechanger.py

--- voicechanger.py
+++ voicechanger.py
@@ -1,62 +1,10 @@
-# import torch
-# from TTS.api import TTS
 
-# # Имитируем старое поведение torch.load (для совместимости)
-# old_torch_load = torch.load
-# def _new_torch_load(*args, **kwargs):
-#     if "weights_only" not in kwargs:
-#         kwargs["weights_only"] = False
-#     return old_torch_load(*args, **kwargs)
-
-# torch.load = _new_torch_load
-
-# # Теперь загружаем модель
-# tts = TTS(
-#     model_name="tts_models/multilingual/multi-dataset/xtts_v2",
-#     progress_bar=True,
-#     gpu=False
-# )
-
-# # Пути к файлам
-# REFERENCE_WAV = "nekopara.wav"  # WAV-файл с голосом для клонирования
-# TEXT_TO_SYNTHESIZE =  "Podozhdite, Chocola edva li mozhet chitat detskie knigi! Ona ne smozhet prochitat recept!"
-# OUTPUT_PATH = "output_cloned_voice.wav"
-
-# print("🔊 Генерация речи...")
-# tts.tts_to_file(
-#     text=TEXT_TO_SYNTHESIZE,
-#     speaker_wav=REFERENCE_WAV,
-#     file_path=OUTPUT_PATH,
-#     split_sentences=True
-# )
-
-# print(f"✅ Переозвученный файл сохранён как {os.path.abspath(OUTPUT_PATH)}")
 import torch
 from TTS.api import TTS
 import warnings
 warnings.filterwarnings("ignore")
 import noisereduce as nr
 import soundfile as sf
-# Обход ошибки с weights_only
-# from rwkv_cpp import RwkvCpp
-
-# # Путь к модели
-# model_path = "rwkv-4-ru-petro-79M-q8_0.gguf"
-
-# # Загрузка модели
-# model = RwkvCpp(model_path=model_path)
-
-# def generate_insult():
-#     prompt = "Скажи что-нибудь постироничное про человека, который спрашивает глупые вопросы:"
-#     output = model.generate(
-#         prompt=prompt,
-#         max_tokens=50,
-#         temperature=0.8,
-#         top_k=50,
-#         top_p=0.95,
-#         repetition_penalty=1.1
-#     )
-#     return output.strip()
 old_torch_load = torch.load
 def _new_torch_load(*args, **kwargs):
     if "weights_only" not in kwargs:
@@ -84,8 +32,6 @@ data, sr = sf.read("output.wav")
 # Удаление шума
 reduced_noise = nr.reduce_noise(y=data, sr=sr)
 
-
-# Сохраняем обратно
 sf.write("output_clean.wav", reduced_noise, sr)
 
 print("✅ Готово!")
